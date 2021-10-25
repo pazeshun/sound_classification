a825453c04c1a6a46bc227ca25d760672fb10492
nakaotatsuya <earthdefense10@gmail.com>
2021-10-25 16:24:40 +0900

    lstm torch

--- scripts/lstm/lstm.py
+++ scripts/lstm/lstm.py
@@ -1,10 +1,10 @@
 #!/usr/bin/env python
 
-#import torch
-#from torch.utils.data import DataLoader
-#import torch.nn as nn
-#import torch.nn.functional as F
-#from torch import optim
+import torch
+from torch.utils.data import DataLoader
+import torch.nn as nn
+import torch.nn.functional as F
+from torch import optim
 
 import chainer
 import chainer.functions as F
@@ -15,6 +15,7 @@ class LSTM(chainer.Chain):
     def __init__(self, n_class=1000):
         super(LSTM, self).__init__()
         insize = 154587
+        #insize = 3 * 227
         hidden_size = 64
         self.n_class = n_class
         with self.init_scope():
@@ -23,8 +24,9 @@ class LSTM(chainer.Chain):
 
     def forward(self, x, t=None):
         h = self.l1(x)
+        #print(h.shape)
         h = self.l2(h)
-        print(h.shape)
+        #print(h.shape)
 
         self.pred = F.softmax(h)
         if t is None:
@@ -72,18 +74,37 @@ class LSTM_2(chainer.Chain):
         chainer.report({"loss": self.loss}, self)
         return self.loss
 
-# class LSTM(nn.Module):
-#     def __init__(self, input_size, hidden_size, n_class=2):
-#         super(LSTM, self).__init__()
-#         self.n_class = n_class
-#         self.rnn = nn.LSTM(
-#             input_size = input_size,
-#             hidden_size = hidden_size,
-#             batch_first = True,
-#             )
-#         self.output = nn.Linear(hidden_size, n_class)
+class LSTM_torch(nn.Module):
+    def __init__(self,  n_class=2):
+        super(LSTM_torch, self).__init__()
+        self.seq_len = 227
+        self.feature_size = 227 * 3
+        self.hidden_layer_size = 128
+        self.rnn_layers = 1
 
-#     def forward(self, x, h=None):
-#         output, hn = self.rnn(x, h)
-#         y = self.output(output[:, -1, :])
-#         return y
+        self.n_class = n_class
+        #print(self.n_class)
+        self.simple_rnn = nn.LSTM(input_size = self.feature_size,
+                                 hidden_size = self.hidden_layer_size,
+                                 num_layers = self.rnn_layers)
+        self.fc = nn.Linear(self.hidden_layer_size, self.n_class)
+
+    def init_hidden(self, batch_size):
+        hidden = torch.zeros(self.rnn_layers, batch_size, self.hidden_layer_size)
+        cell = torch.zeros(self.rnn_layers, batch_size, self.hidden_layer_size)
+        return (hidden, cell)
+    
+    def forward(self, x, h=None):
+        #print(x.shape)
+        batch_size = x.shape[0]
+        self.hidden_cell = self.init_hidden(batch_size)
+        self.hidden = self.hidden_cell[0].to("cuda")
+        self.cell = self.hidden_cell[1].to("cuda")
+        x = x.view(batch_size, self.feature_size, self.seq_len) #(batch, channel, height, width) > (batch, height(3channel), width) = (batch, feature, sequence)
+        x = x.permute(2,0,1)
+        #print(x.shape)
+
+        output, (h_n, c_n) = self.simple_rnn(x, (self.hidden, self.cell)) # RNN input - (seq, batch, feature)
+        x = h_n[-1,:,:]
+        x = self.fc(x)
+        return x
--- scripts/train_torch.py
+++ scripts/train_torch.py
@@ -4,9 +4,11 @@ import argparse
 
 import torch
 import torch.nn as nn
+import torch.optim as optim
 from torch.utils.data import DataLoader, Dataset
 import torch.nn.functional as F
 import torchvision
+import  torchvision.transforms as transforms
 
 import matplotlib
 import numpy as np
@@ -17,12 +19,12 @@ import rospkg
 import rospy
 
 from nin.nin import NIN
-from lstm.lstm import LSTM
+from lstm.lstm import LSTM_torch
 
 matplotlib.use("Agg")
 
 class PreprocessedDataset(torch.utils.data.Dataset):
-    def __init__(self, path=None, random=True, train_data="train_data"):
+    def __init__(self, path=None, random=True, transform= None, train_data="train_data"):
         rospack = rospkg.RosPack()
         self.root = osp.join(rospack.get_path(
             "sound_classification"), train_data)
@@ -33,6 +35,7 @@ class PreprocessedDataset(torch.utils.data.Dataset):
         # how many classes to be classified
         self.n_class = 0
         self.target_classes = []
+        self.transform = transform
         with open(osp.join(self.root, 'n_class.txt'), mode='r') as f:
             for row in f:
                 self.n_class += 1
@@ -40,22 +43,26 @@ class PreprocessedDataset(torch.utils.data.Dataset):
         # Load mean image of dataset
         mean_img_path = osp.join(rospack.get_path('sound_classification'),
                                  train_data, 'dataset', 'mean_of_dataset.png')
-        mean = np.array(Image_.open(mean_img_path), np.float32).transpose(
-            (2, 0, 1))  # (height, width, channel) -> (channel ,height, width), rgb
+        # mean = np.array(Image_.open(mean_img_path), np.float32).transpose(
+        #     (2, 0, 1))  # (height, width, channel) -> (channel ,height, width), rgb
         #self.mean = mean.astype(chainer.get_dtype())
+
+        mean = np.array(Image_.open(mean_img_path), np.float32)  # (height, width, channel)
         self.mean = mean
+        
 
     def __len__(self):
         return len(self.base)
 
     def __getitem__(self, i):
         image, label = self.base.samples[i]
-        image = np.array(Image_.open(base.samples[0][0]), dtype=np.float32).transpose((2,0,1))
-        label = np.array(label, dtype=np.int32)
+        #a = np.array(Image_.open(self.base.samples[i][0]))
+        #print(a.shape) #(227, 227, 3)
+        image = np.array(Image_.open(self.base.samples[i][0]), dtype=np.float32)
+        label = np.array(label, dtype=np.float32)
         image = self.process_image(image)
 
-        image = torch.Tensor(image, dtype=torch.float)
-        label = torch.Tensor(label, dtype=torch.float)
+        image = self.transform(image)
         return image, label
 
     def process_image(self, image):
@@ -67,7 +74,7 @@ def load_model(model_name, n_class):
     archs = {
         #'nin': NIN,
         #'vgg16': VGG16BatchNormalization
-        "lstm": LSTM
+        "lstm": LSTM_torch
     }
     model = archs[model_name](n_class=n_class)
     return model
@@ -88,11 +95,12 @@ def main():
     args = parser.parse_args()
 
 
+    trans1 = transforms.ToTensor()
     if args.gpu == -1:
         device = "cpu"
     elif args.gpu == 0:
         device = "cuda"
-    #device = chainer.cuda.get_device_from_id(args.gpu)  # for python2
+        
     batchsize = 32
     # Path to training image-label list file
     #train_labels = osp.join(rospack.get_path('sound_classification'),
@@ -109,19 +117,72 @@ def main():
     print('epoch: {}'.format(args.epoch))
     print('')
 
-    train = PreprocessedDataset(path="dataset_torch", train_data=args.train_data)
-    val = PreprocessedDataset(path="dataset_torch_val", val_labels, False, train_data=args.train_data)
+    train = PreprocessedDataset("dataset_torch", transform = trans1, train_data=args.train_data)
+    #print(train.__getitem__(0)[0].size())
+    #print(train.__getitem__(0)[1])
+    #train.__getitem__(0)
+    val = PreprocessedDataset("dataset_torch_val", transform = trans1, train_data=args.train_data)
 
     model = load_model(args.model, train.n_class)
     model = model.to(device)
 
-    trainloader = torch.utils.data.DataLoader(train, batch_size=batchsize, shuffle=False, num_workers=2)
-    testloader = torch.utils.data.DataLoader(val, batch_size=batchsize, shuffle=False, num_workers=2)
+    trainloader = torch.utils.data.DataLoader(train, batch_size=batchsize, shuffle=False, num_workers=1)
+    testloader = torch.utils.data.DataLoader(val, batch_size=batchsize, shuffle=False, num_workers=1)
+
+    dataloader_dict = {
+        "train": trainloader,
+        "test": testloader
+    }
 
     optimizer = optim.SGD(model.parameters(), lr=0.01, momentum=0.9)
+    criterion = nn.CrossEntropyLoss()
 
     out = osp.join(rospack.get_path("sound_classification"),
                    args.train_data, "result_torch", args.model)
     if not osp.exists(out):
         makedirs(out)
 
+    #batch_iterator = iter(dataloader_dict["train"])
+    #inputs, labels = next(batch_iterator)
+    #print(inputs.size())
+    #print(labels)
+
+    num_epochs = 30
+    for epoch in range(num_epochs):
+        print("-----")
+
+        for phase in ["train", "test"]:
+            if phase == "train":
+                model.train()
+            else:
+                model.eval()
+
+            epoch_loss = 0.0
+            epoch_corrects = 0
+
+            for inputs, labels in dataloader_dict[phase]:
+                optimizer.zero_grad()
+                inputs = inputs.to(device)
+                labels = labels.type(torch.LongTensor)
+                with torch.set_grad_enabled(phase == "train"):
+                    outputs = model(inputs)
+                    outputs = outputs.to("cpu")
+                    #calculate loss
+                    loss = criterion(outputs, labels)
+                    #predict labels
+                    _, preds = torch.max(outputs, 1)
+
+                    if phase == "train":
+                        loss.backward()
+                        optimizer.step()
+                    epoch_loss += loss.item() * inputs.size(0)
+                    epoch_corrects += torch.sum(preds == labels.data)
+
+            #loss and accuracy for each epoch
+            epoch_loss = epoch_loss / len(dataloader_dict[phase].dataset)
+            epoch_acc = epoch_corrects.double() / len(dataloader_dict[phase].dataset)
+
+            print("{} Loss: {:.4f} Acc: {:.4f}".format(phase, epoch_loss, epoch_acc))
+
+if __name__ == "__main__":
+    main()
